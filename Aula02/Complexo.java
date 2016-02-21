f100ad35c0b809d55d2e2214584a497d58be5faa
cfchenr <c.henriques@ua.pt>
2016-02-21 01:57:03 +0000

    Exercícios resolvidos aula02

---
+++ Aula02/Complexo.java
@@ -0,0 +1,27 @@
+
+public class Complexo {
+	private double re;
+	private double im;
+
+	public Complexo(double real, double imag) {
+		re = real;
+		im = imag;
+	}
+
+	public double real() {
+		return re;
+	} 
+
+	public double imag() {
+		return im;
+	} 
+
+	public double abs() {
+		return Math.sqrt(re * re + im * im);
+	} 
+
+	public double phase() {
+		return Math.atan2(im, re);
+	} 
+
+}
