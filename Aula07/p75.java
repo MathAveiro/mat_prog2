461879bcb2ab7d218580e6e7329ef755f7124c90
Fábio Henriques <fabiomchenriques@ua.pt>
2016-03-21 00:45:34 +0000

    Exercícios resolvidos aula 07

---
+++ Aula07/p75.java
@@ -0,0 +1,38 @@
+import static java.lang.System.*;
+
+public class p75
+{
+   public static void main(String[] args)
+   {
+      double number = 0.0, epsilon=0.0, init=1.0;
+
+      try {
+         number = Double.parseDouble(args[0]);
+         epsilon = Double.parseDouble(args[1]);
+         init = Double.parseDouble(args[2]);
+      }
+      catch (ArrayIndexOutOfBoundsException e) {
+         out.println("ERRO: número de argumentos insuficiente");
+         out.println("Uso: java -ea p75 <number> <epsilon> <init>");
+         exit(1);
+      }
+      catch (NumberFormatException e) {
+         out.println("ERRO: formato inválido");
+         out.println("Uso: java -ea p75 <number> <epsilon> <init>");
+         exit(1);
+      }
+
+      double root = squareRoot(number,epsilon,init);
+
+      out.println("Raiz quadrada de "+number+": "+root);
+
+   }
+
+   static double squareRoot(double number, double epsilon, double initRoot)
+   {
+      // Completar
+      return 0.0;
+   }
+} 
+
+
