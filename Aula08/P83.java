6564a391e02c06ad2ed0246d1ac7b3a67a971dc7
cfchenr <c.henriques@ua.pt>
2016-04-11 02:19:29 +0100

    Exercícios resolvidos aula08

---
+++ Aula08/P83.java
@@ -0,0 +1,33 @@
+
+import static java.lang.System.*;
+import java.io.*;
+import p2utils.*;
+
+public class P83
+{
+   // Introduz numa lista ordenada
+   // os numeros inteiros dados na linha de comando
+   // e verifica se os números 1, 10 e 30 pertencem à lista
+
+
+   public static void main(String[] args) throws IOException
+   {
+      SortedList<Integer> lst = new SortedList<Integer>();
+
+      for(int i=0; i<args.length; i++) {
+         lst.insert(Integer.parseInt(args[i]));
+      }
+
+      out.println(lst.contains(1));
+      out.println(lst.contains(10));
+      out.println(lst.contains(30));
+      // acrescentar mais testes se necessário
+      
+      while(!lst.isEmpty()) {
+         out.println(lst.first());
+         lst.removeFirst();
+      }
+   }
+}
+
+
