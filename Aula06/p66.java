7e2efa0f889e5f8d15f8ef0f686bd5746e9de757
cfchenr <c.henriques@ua.pt>
2016-05-20 15:40:03 +0100

    remove

--- Aula06/p66.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-import java.io.*;
-import static java.lang.System.*;
-public class p66 {
-	public static void main(String[] args) {
-		
-	}
-}
--- GuiãoTA/E15/test1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -ea MainTrain 10 100 1 2 3 R R 4.5 0.1
