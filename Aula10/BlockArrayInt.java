13700d27724dc747651367524381d3c90bfb8cd0
fabioHfab <fabiomchenriques@ua.pt>
2016-05-20 16:02:57 +0100

    Exercícios resolvidos aula10

--- Aula10/BlockArrayInt.java
+++ Aula10/BlockArrayInt.java
@@ -1,40 +1,52 @@
+import p2utils.*;
 public class BlockArrayInt
 {
+   private int bS, nB;
+   LinkedList<int[]> list = new LinkedList<int[]>();
    public BlockArrayInt(int blockSize,int numBlocks)
    {
       assert blockSize > 0;
       assert numBlocks > 0;
-
+      bS=blockSize;
+      nB=numBlocks;
       a = new int[blockSize*numBlocks];
+      list.addLast(a);
    }
 
    public int get(int index)
    {
       assert validIndex(index);
-
-      return a[index];
+      //System.out.println(list.size());
+      int block = (int)( index / bS );
+      int i = index - (block * bS);
+      a = list.get(block);
+      return a[i];
    }
 
    public void put(int elem,int index)
    {
       assert validIndex(index);
-
-      a[index] = elem;
+      int block = (int)( index / bS );
+      //System.out.println(block);
+      int i = index - (block * bS);
+      a[i] = elem;
    }
 
    public void incrementNumberOfBlocks()
-   {
-      assert false: "Not yet implemented!";
+   {  
+      nB++;
+      a = new int[bS];
+      list.addLast(a);
    }
 
    public int size()
    {
-      return a.length;
+      return nB*bS;
    }
 
    public int numberOfBlocks()
    {
-      return 1;
+      return nB;
    }
 
    public int blockSize()
@@ -44,7 +56,7 @@ public class BlockArrayInt
 
    public boolean validIndex(int index)
    {
-      return index >= 0 && index < size();
+      return index >= 0 && index < bS*(nB+1);
    }
 
    private int[] a;
