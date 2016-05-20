60bf926e70cedb368c4e7ae319b9dfc84d3ba45f
fabioHfab <fabiomchenriques@ua.pt>
2016-05-20 17:45:33 +0100

    Exercícios resolvidos aula12

---
+++ Aula12/p2utils/KeyValueNode.java
@@ -0,0 +1,26 @@
+package p2utils;
+
+class KeyValueNode<E> {
+
+   final String key;
+   E elem;
+   KeyValueNode<E> next;
+
+   KeyValueNode(String k,E e,KeyValueNode<E> n) {
+      key  = k;
+      elem = e;
+      next = n;
+   }
+
+   KeyValueNode(String k,E e) {
+      key  = k;
+      elem = e;
+      next = null;
+   }
+
+   // a fazer para o ex. 11.3
+   public String toString() {
+      return "("+ key + "," + elem.toString() + ")";
+   }
+}
+
