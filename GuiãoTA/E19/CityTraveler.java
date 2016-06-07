c4a233793327ac98bd4a15f845a05ca7520358f9
cfchenr <c.henriques@ua.pt>
2016-06-07 18:43:15 +0100

    Exercícios resolvidos do guião de trabalho autónomo

--- GuiãoTA/E19/CityTraveler.java
+++ GuiãoTA/E19/CityTraveler.java
@@ -13,22 +13,21 @@ public class CityTraveler
 
 		String[] keys = visitas.keys();
 		for(int i = 0; i < keys.length; i++) {
-			out.println(keys[i] + ": " + visitas.get(keys[i]));
+			out.printf("%s: %s\n", keys[i], visitas.get(keys[i]));
 		}
 	}
 
 	public static void addVisitas(HashTable<String> visitas, String cidade)throws IOException {
 		File f = new File(cidade);
 		Scanner scf = new Scanner(f);
-		String[] parts = cidade.split("[\\W]");
 		while(scf.hasNextLine()) {
 			String nome = scf.nextLine();
 			String temp = "";
 			if(visitas.contains(nome)) {
 				temp = visitas.get(nome);
-				visitas.set(nome, temp+" "+parts[0]);
+				visitas.set(nome, temp+" "+cidade);
 			}
-			else visitas.set(nome, parts[0]);
+			else visitas.set(nome, cidade);
 		}
 	}
 }
