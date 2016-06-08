4185d281a450faeb54207b52858aacf0125e1e4b
cfchenr <c.henriques@ua.pt>
2016-06-08 14:50:25 +0100

    Exercícios resolvidos do guião de trabalho autónomo

--- GuiãoTA/E19/CityTraveler.java
+++ GuiãoTA/E19/CityTraveler.java
@@ -2,7 +2,6 @@ import static java.lang.System.*;
 import java.util.Scanner;
 import java.io.*;
 import p2utils.*;
-
 public class CityTraveler
 {
 	public static void main(String[] args)throws IOException {
